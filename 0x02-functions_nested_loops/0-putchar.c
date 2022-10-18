3b4c03b9c1d34fceba8f2b7cb38f178022e45ef9
kouamojulesquentin <kouamojulesquentin@gmail.com>
2022-10-18 06:34:36 -0700

    first commit in this repo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include<sdtio>
+#include"main.h"
 
-\**
+/**
 * main : print _putchar
 * Return
-*\
+*/
 
 int main(void){
 
-printf("_putchar");
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+
 Return (0);
   
 }
