2fdc3b54b0ee46baefbb68bad2651bfb24b571e7
kouamojulesquentin <kouamojulesquentin@gmail.com>
2022-10-21 07:12:09 -0700

    zz

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,13 @@ printf("Infinite loop incoming :(\n");
 
  i = 0;
 
- while (i < 10)
- {
- putchar(i);/* infinite loop */
- }
-
+ /**
+  *while (i < 10)
+  *{
+  *Infinite loop - No increase of variable
+  *putchar(i);
+}
+ */
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
